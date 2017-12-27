df196045857a538c87365fe401772df138ce2010
Jose Manuel Corral <jose.corral@tokiota.com>
2017-12-27 08:26:02 +0100

    WIP

--- cli.js
+++ cli.js
@@ -8,27 +8,25 @@ const [,, ... args] = process.argv
 var inquirer = require('inquirer');
 var git = require('simple-git');
 
-
-var localBranches; 
-git().branchLocal((listOfBranches) => {
-    console.log(JSON.stringify(listOfBranches));
-});
-
-
-
-var questions = [
-    {
-        type: 'list',
-        name: 'branch',
-        message: 'Select the branch you want to work',
-        choices: ['Large', 'Medium', 'Small'],
-        filter: function(val) {
-          return val.toLowerCase();
+var localBranches = [];
+git().branchLocal((errors, branchSummary) => {
+    console.log(`Current Branch ${branchSummary.current}`);
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'branch',
+            message: 'Select the branch you want to work',
+            choices: branchSummary.all,
+            filter: function(val) {
+              return val;
+            }
         }
-    }
-];
+    ]).then(answers => {
+        console.log('\nBranch Selected:');
+        git().checkoutBranch(answers.branch, startPoint, () => {});
+        console.log(JSON.stringify(answers, null, '  '));
+    });
 
-inquirer.prompt(questions).then(answers => {
-    console.log('\nBranch Selected:');
-    console.log(JSON.stringify(answers, null, '  '));
 });
+
+
